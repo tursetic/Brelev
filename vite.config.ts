6b23e0321520a3fc07b9d79a431068e5a6f765f8
tursetic <tursetic@gmail.com>
2026-06-12 13:19:28 +0900

    Update vite.config.ts
    CloudFlare 도입으로 base brelev 필요 없음

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ★★★ 아래 base 설정은 절대로 허가 없이 지우지 마세요! github 배포와 관련 있습니다.
-  base: '/Brelev/', 
   server: {
     proxy: {
       '/geoserver': {
